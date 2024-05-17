fdfcf3ea7cfd5f1c32d1660299865569400964d7
thiago araujo <devthiagoaraujo@gmail.com>
2024-05-17 00:59:45 -0300

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./routes/Home.jsx";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     component: App,
-  }
 
-])
+    children: [{ path: "/", element: <Home /> }],
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
